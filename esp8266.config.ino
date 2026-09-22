64f91226e47b6870bdb4674b8c6ed6e85f0ede2d
Nikan <nik2006majid@gmail.com>
2026-09-22 19:01:11 +0330

    Define the new function for wifi connection and show it

--- esp8266.config.ino
+++ esp8266.config.ino
@@ -10,3 +10,21 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 const char* topic = "TOPIC";
+
+void setup_wifi() {
+  delay(10);
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println();
+  Serial.println("WiFi connected!");
+  Serial.println(WiFi.localIP());
+}
+
