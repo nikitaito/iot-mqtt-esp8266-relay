4015d3265bb6cfedc3c249be2c52a1cf3619f59b
Nikan <nik2006majid@gmail.com>
2026-09-22 19:05:44 +0330

    Define the new function for getting the information from the mqtt server

--- esp8266.config.ino
+++ esp8266.config.ino
@@ -28,3 +28,12 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received: ");
+
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+
+  Serial.println();
+}
