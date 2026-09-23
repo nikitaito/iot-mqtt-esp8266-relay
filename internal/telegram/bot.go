1e3346288c01e1c7fa389222a3e20c46c43e6bc7
Nikan <nik2006majid@gmail.com>
2026-09-23 18:12:36 +0330

    Creating the Bot struct

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -1,1 +1,13 @@
 package telegram
+
+import (
+	"github.com/go-telegram/bot"
+	"github.com/nikitaito/iot-mqtt-esp8266-relay/internal/config"
+	"github.com/nikitaito/iot-mqtt-esp8266-relay/internal/device"
+)
+
+type Bot struct {
+	api   *bot.Bot
+	cfg   *config.Config
+	relay *device.Relay
+}
