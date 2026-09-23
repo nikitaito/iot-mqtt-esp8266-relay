6f7424467ff53f9cb836fa3393851f8bf166bd46
Nikan <nik2006majid@gmail.com>
2026-09-23 18:17:38 +0330

    Adding the new function for make the Bot struct

--- internal/telegram/bot.go
+++ internal/telegram/bot.go
@@ -11,3 +11,26 @@ type Bot struct {
 	cfg   *config.Config
 	relay *device.Relay
 }
+
+func New(cfg *config.Config, api *bot.Bot, relay *device.Relay) (*Bot, error) {
+	b := &Bot{
+		cfg:   cfg,
+		relay: relay,
+	}
+
+	opts := []bot.Option{
+		bot.WithDefaultHandler(b.handleUnknown),
+	}
+
+	api, err := bot.New(cfg.TelegramBotToken, opts...)
+	if err != nil {
+		return nil, err
+	}
+	b.api = api
+
+	api.RegisterHandler(bot.HandlerTypeMessageText, "/start", bot.MatchTypeExact, b.handleStart)
+	api.RegisterHandler(bot.HandlerTypeMessageText, "/on", bot.MatchTypeExact, b.handleOn)
+	api.RegisterHandler(bot.HandlerTypeMessageText, "/off", bot.MatchTypeExact, b.handleOff)
+
+	return b, nil
+}
