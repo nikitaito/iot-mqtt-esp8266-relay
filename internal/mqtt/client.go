639a1b40e1a031ba823dd378e976626d35b90cd2
Nikan <nik2006majid@gmail.com>
2026-09-23 15:36:41 +0330

    Initial defining of the New function for the make client struct

--- internal/mqtt/client.go
+++ internal/mqtt/client.go
@@ -1,10 +1,26 @@
 package mqtt
 
 import (
+	"time"
+
 	paho "github.com/eclipse/paho.mqtt.golang"
+	"github.com/nikitaito/iot-mqtt-esp8266-relay/internal/config"
 )
 
 type Client struct {
 	pahoClient paho.Client
 	topic      string
+}
+
+func New(cfg *config.Config) (*Client, error) {
+	opts := paho.NewClientOptions().
+		AddBroker(cfg.MQTTBrokerURL).
+		SetClientID(cfg.MQTTClientID).
+		SetUsername(cfg.MQTTUsername).
+		SetPassword(cfg.MQTTPassword).
+		SetAutoReconnect(true).
+		SetConnectTimeout(10 * time.Second).
+		SetConnectRetry(true).
+		SetConnectRetryInterval(5 * time.Second)
+
 }
