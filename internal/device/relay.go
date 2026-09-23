f505d0fe45086dd21834fd1453d3a7fd34e084ca
Nikan <nik2006majid@gmail.com>
2026-09-23 16:21:11 +0330

    define the TurnON function for turn relay on

--- internal/device/relay.go
+++ internal/device/relay.go
@@ -5,3 +5,8 @@ import "github.com/nikitaito/iot-mqtt-esp8266-relay/internal/mqtt"
 type Relay struct {
 	mqttClient *mqtt.Client
 }
+
+func (rel *Relay) TurnON() error {
+	err := rel.mqttClient.Publish("Relay_ON")
+	return err
+}
